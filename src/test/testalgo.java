03a6fd32a09244aa9490d6866d8cfabd47d160b4
cathylass <cathy.lassalle31@gmail.com>
2019-06-28 15:00:53 +0200

    testalgo

---
+++ src/test/testalgo.java
@@ -0,0 +1,27 @@
+package test;
+
+public class testalgo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   
+		int tab[]= {-20,-30,-17};
+		int big = biggest(tab);
+
+		System.out.println ("max = "+ big);
+	}
+
+	private static int biggest(int[] tab) {
+		// TODO Auto-generated method stub
+		int max=Integer.MIN_VALUE;
+		
+		for(int i=0;i<tab.length;i++) {
+			if(tab[i]>max)
+				max=tab[i];
+		}
+		
+		
+		return max;
+	}
+     
+}
